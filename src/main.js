630324bcb808f5bd96c32a1f3671c897bd8ff344
조유진 <s2570@e-mirim.hs.kr>
2026-06-11 16:45:29 +0900

    회원가입 페이지

--- src/main.js
+++ src/main.js
@@ -1,4 +1,35 @@
 import "./style.css";
 import { supabase } from "./supabase.js"; //다른 파일에서 supabase를 가져온다
 
-console.log("홈 페이지 로드됨");
+const cafeList = document.querySelector("#cafeList");
+
+async function loadCafes() {
+  const { data, error } = await supabase.from("cafes").select("*");
+
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  cafeList.innerHTML = "";
+
+  data.forEach((cafe) => {
+    cafeList.innerHTML += `
+            <div class="cafe-card">
+                <div class="cafe-image"></div>
+
+                <div class="cafe-info">
+                    <div class="cafe-title">
+                        ${cafe.name}
+                    </div>
+
+                    <div class="cafe-desc">
+                        ${cafe.description}
+                    </div>
+                </div>
+            </div>
+        `;
+  });
+}
+
+loadCafes();
