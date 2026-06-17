8c79ee94960767b9178e255280e20de678fb8496
섭C2도 <leecs1404@gmail.com>
2026-06-17 21:04:01 +0900

    카페 가입 구현 / 카페 목록 구현 / 가입된 카페 목록 구현 / 카페 오너 이름 표시 구현 / 카페 생성 개수 제한 구현 / 로그인 유무 카페 가입 실패 확인 필요 / CSS 구현 꽤 많이 필요

--- src/auth.js
+++ src/auth.js
@@ -19,6 +19,84 @@ async function checkLogin() {
     window.location.href = "./create-cafe.html";
   });
 
+  const cafeList = document.getElementById("cafeList");
+
+  const { data: cafes } = await supabase.from("cafes").select(`
+    *,
+    profiles!cafes_owner_id_fkey (
+      nickname
+    )
+  `);
+
+  cafeList.innerHTML = "";
+
+  cafes.forEach((cafe) => {
+    cafeList.innerHTML += `
+  <div class="cafe-card" data-id="${cafe.id}">
+    <div class="cafe-info">
+      <div class="cafe-title">${cafe.name}</div>
+
+      <div class="cafe-desc">
+        ${cafe.description ?? ""}
+      </div>
+
+      <div class="cafe-owner">
+        카페 주인 : ${cafe.profiles.nickname}
+      </div>
+    </div>
+  </div>
+`;
+  });
+
+  document.querySelectorAll(".cafe-card").forEach((card) => {
+    card.addEventListener("click", async () => {
+      const cafeId = card.dataset.id;
+
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (!user) {
+        alert("카페에 가입하려면 로그인해주세요.");
+        window.location.href = "./login.html";
+        return;
+      }
+
+      const result = confirm("이 카페에 가입하시겠습니까?");
+
+      if (!result) {
+        return;
+      }
+
+      const { data: alreadyJoined } = await supabase
+        .from("cafe_members")
+        .select("*")
+        .eq("cafe_id", cafeId)
+        .eq("user_id", user.id)
+        .maybeSingle();
+
+      if (alreadyJoined) {
+        alert("이미 가입한 카페입니다.");
+        return;
+      }
+
+      const { error } = await supabase.from("cafe_members").insert({
+        cafe_id: cafeId,
+        user_id: user.id,
+      });
+
+      if (error) {
+        alert(error.message);
+        return;
+      }
+
+      alert("카페 가입 완료!");
+      location.reload();
+
+      // 가입 처리
+    });
+  });
+
   const {
     data: { user },
   } = await supabase.auth.getUser();
@@ -46,8 +124,55 @@ async function checkLogin() {
 
   const joinedBox = document.getElementById("joined-box");
 
-  if (user) {
-    joinedBox.textContent = "가입된 카페가 없습니다.";
+  const { data: memberships } = await supabase
+    .from("cafe_members")
+    .select("cafe_id")
+    .eq("user_id", user.id);
+
+  if (!memberships || memberships.length === 0) {
+    joinedBox.innerHTML = `
+    <div class="joined-empty">
+      가입된 카페가 없습니다.
+    </div>
+  `;
+  } else {
+    const cafeIds = memberships.map((m) => m.cafe_id);
+
+    const { data: joinedCafes } = await supabase
+      .from("cafes")
+      .select(
+        `
+    *,
+    profiles!cafes_owner_id_fkey (
+      nickname
+    )
+  `,
+      )
+      .in("id", cafeIds);
+
+    joinedBox.innerHTML = "";
+
+    const ownedCafes = joinedCafes.filter((cafe) => cafe.owner_id === user.id);
+
+    const memberCafes = joinedCafes.filter((cafe) => cafe.owner_id !== user.id);
+
+    ownedCafes.forEach((cafe) => {
+      joinedBox.innerHTML += `<div class="joined-cafe-card">
+  <div>${cafe.name}</div>
+  <div class="cafe-owner">카페 주인 : ${cafe.profiles?.nickname ?? "알 수 없음"}
+  </div>
+</div>
+    `;
+    });
+
+    memberCafes.forEach((cafe) => {
+      joinedBox.innerHTML += `<div class="joined-cafe-card">
+  <div>${cafe.name}</div>
+  <div class="cafe-owner">카페 주인 : ${cafe.profiles?.nickname ?? "알 수 없음"}
+  </div>
+</div>
+    `;
+    });
   }
 }
 
--- src/create-cafe.js
+++ src/create-cafe.js
@@ -7,17 +7,42 @@ createBtn.addEventListener("click", async () => {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const { count, error: countError } = await supabase
+    .from("cafes")
+    .select("*", { count: "exact", head: true })
+    .eq("owner_id", user.id);
+
+  if (count >= 4) {
+    alert("카페는 최대 4개까지 생성할 수 있습니다.");
+    window.location.href = "./index.html";
+    return;
+  }
+
   const name = document.getElementById("cafe-name").value;
   const description = document.getElementById("cafe-description").value;
 
-  const { error } = await supabase.from("cafes").insert({
-    name,
-    description,
-    owner_id: user.id,
-  });
+  const { data: cafeData, error } = await supabase
+    .from("cafes")
+    .insert({
+      name,
+      description,
+      owner_id: user.id,
+    })
+    .select()
+    .single();
 
   if (error) {
     alert(error.message);
+    return;
+  }
+
+  const { error: memberError } = await supabase.from("cafe_members").insert({
+    cafe_id: cafeData.id,
+    user_id: user.id,
+  });
+
+  if (memberError) {
+    alert(memberError.message);
     return;
   }
 
